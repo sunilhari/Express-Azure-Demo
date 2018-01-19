b99ce053ee961b5b6bb464f80b4174645f0437be
Sunil Hari <isunilhari@gmail.com>
2018-01-19 08:41:40 +0530

    Updated with azure funtions

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    azure = require('azure-storage'),
     port = 3000;
 
 
-var app = express();
+var app = express(),
+    blobSvc = azure.createBlobService();
 app.use(bodyParser.json())
     .use(bodyParser.urlencoded({
         extended: true
     }))
     .post('/feedback', function(request, response) {
         console.log(request.body.recbrnd);
-        response.send('Store Number:'+request.body.strnbr);
+        var blob = blobSvc.listBlobsSegmented('appfiles', null, function(error, result, response) {
+            if (!error) {
+                // result.entries contains the entries
+                // If not all blobs were returned, result.continuationToken has the continuation token.
+                response.write('<p>Blob Token</p>');
+            }
+            else{
+                response.send('Error')
+            }
+
+        });
     })
     .listen(port, function() {
         console.log('Server Started at ' + port);
