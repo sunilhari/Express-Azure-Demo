d51acdb9393b3d76f5c2966c7f41d0a9767aee52
Sunil Hari <isunilhari@gmail.com>
2018-01-19 12:02:07 +0530

    Templating Support.News API Added

--- server.js
+++ server.js
@@ -1,29 +1,54 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     azure = require('azure-storage'),
-    port = 3000;
+    port = 3000,
+    path = require('path'),
+    hbs = require('express-handlebars'),
+    axios = require('axios'),
+    newsURL = "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=97e945c75f414d02934d3b5705286304"
 
 
-var app = express(),
-    blobSvc = azure.createBlobService();
+var app = express();
+app.engine('handlebars', hbs({
+    defaultLayout: 'index'
+}));
+app.set('view engine', 'handlebars');
 app.use(bodyParser.json())
     .use(bodyParser.urlencoded({
         extended: true
     }))
-    .post('/feedback', function(request, response) {
-        console.log(request.body.recbrnd);
-        var blob = blobSvc.listBlobsSegmented('appfiles', null, function(error, result, response) {
-            if (!error) {
-                // result.entries contains the entries
-                // If not all blobs were returned, result.continuationToken has the continuation token.
-                response.send('<p>Blob Token</p>');
-            }
-            else{
-                response.send('Error')
-            }
-
-        });
+    .get('/', function (request, response) {
+        response.sendFile(path.join(__dirname + '/index.html'));
     })
-    .listen(port, function() {
+    .post('/feedback', function (request, parentResponse) {
+        console.log('Feedback');
+        axios
+            .get(newsURL)
+            .then(function (response) {
+                console.log('Response received from News API'+request.body.recbrnd);
+                console.log(request);
+                parentResponse.render('response', {
+                    articles: response.data.articles,
+                    storeNum:request.body.strnbr,
+                    brand:request.body.recbrand
+                });
+            })
+            .catch(function (error) {
+                parentResponse.render('error');
+            });
+    })
+    .get('/news', function (request, parentResponse) {
+        axios
+            .get(newsURL)
+            .then(function (response) {
+                parentResponse.render('response', {
+                    articles: response.data.articles
+                });
+            })
+            .catch(function (error) {
+                parentResponse.render('error');
+            });
+    })
+    .listen(port, function () {
         console.log('Server Started at ' + port);
     });
